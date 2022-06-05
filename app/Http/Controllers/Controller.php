9c42b848412a88d2d9dda64056a1e97abb5e56ef
Anyi Lozano <anyisitha12@gmail.com>
2022-06-05 01:30:31 -0500

    Adding .env

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,25 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function responseApi($status, $message = [], $data = [])
+    {
+        if($status["type"] === "success"){
+            $message = $message;
+            $message["code"] = 200;
+        }else if($status["type"] === "error"){
+            $message = $message;
+            $message["code"] = 500;
+        }else if($status["type"] === "warning"){
+            $message = $message;
+            $message["code"] = 300;
+        }else if($status["type"] === "not found"){
+            $message = $message;
+            $message["code"] = 404;
+        }else{
+            abort(500);
+        }
+
+        return ["transaction" => ["status" => $status], $message, $data];
+    }
 }
