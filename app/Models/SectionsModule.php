b1b305dc2b480f344774fcd1102e8bc6470fa2fa
Anyi Lozano <anyisitha12@gmail.com>
2022-06-06 00:12:02 -0500

    Changes

--- app/Models/SectionsModule.php
+++ app/Models/SectionsModule.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class SectionsModule extends Model
 {
     use HasFactory;
+
+    public function content()
+    {
+        return $this->hasMany(ContentSectionsModule::class, "section_module_id");
+    }
 }
